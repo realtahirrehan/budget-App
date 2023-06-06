4272d093738730a84e261bcdaab0dad562d5e8b4
Tahir Rehan <tahirrehankhanzada2017@gmail.com>
2023-06-07 03:23:56 +0500

    fully functional

--- app.js
+++ app.js
@@ -20,34 +20,57 @@ var expenseTableBody = document.getElementById("expense-table-body");
 var allExpenses = [];
 
 addExpenseButton.addEventListener("click", function() {
-    var category = document.getElementById("category");
-    var description = document.getElementById("description");
-    var amount = document.getElementById("amount");
-    var date = document.getElementById("date");
+
+    var total = +document.getElementById("get").value
+    var category = document.getElementById("category").value;
+    var description = document.getElementById("description").value;
+    var amount = document.getElementById("amount").value;
+    var date = document.getElementById("date").value;
+
+    if (total<=0) {
+        alert("Please enter a total budget");
+        return
+    }
+
+    if (!category || !description || !amount || !date ) {
+       alert("Please enter complete information");
+       return
+    }
+
+    if (amount > total || amount > balanceOutput) {
+        alert("Your cannot spend more than your budget");
+        return
+    }
+    if (amount <= 0) {
+        alert("Please enter amount greater than 0");
+        return
+    }
 
     var expense = {
-        categoryprop: category.value,
-        descriptionprop: description.value,
-        amountprop: parseFloat(amount.value),
-        dateprop: date.value
+        categoryprop: category,
+        descriptionprop: description,
+        amountprop: parseFloat(amount),
+        dateprop: date
     }
 
     allExpenses.push(expense);
-
-    createExpensesRow(expense);
     
     const inputs = document.querySelectorAll('#category, #description, #amount, #date');
     inputs.forEach(input => {
     input.value = '';
     }); 
 
-    var sum = allExpenses.reduce(function(total, expenses) {
-        return total + expenses.amountprop;
+    var sum = allExpenses.reduce(function(total2, expenses) {
+        return total2 + expenses.amountprop;
     }, 0);
     var total = parseFloat(totalBudget.value);
     expenseOutput.innerText = sum; 
     balanceOutput = total - sum;
     balance.innerText = balanceOutput;
+    addExpenseButton.innerText = "Add Expense"
+
+    createExpensesRow(expense);
+    
 
 })
 
@@ -57,7 +80,6 @@ function createExpensesRow(expense) {
     var categoryCell = document.createElement("td");
     categoryCell.textContent = expense.categoryprop;
     row.appendChild(categoryCell);
-
     var descriptionCell = document.createElement("td");
     descriptionCell.textContent = expense.descriptionprop;
     row.appendChild(descriptionCell);
@@ -74,18 +96,38 @@ function createExpensesRow(expense) {
     var editButton = document.createElement("button");
     editButton.textContent = "Edit";
     editButton.classList.add("edit");
+
     editButton.addEventListener("click", function() {
-        console.log("Edit expense:", expense);
+    
+    var expenseIndex = allExpenses.findIndex(exp => exp === expense);
+    
+    var category = document.getElementById("category").value = expense.categoryprop;
+    var description = document.getElementById("description").value = expense.descriptionprop;
+    var amount = document.getElementById("amount").value = expense.amountprop;
+    var date = document.getElementById("date").value = expense.dateprop;
+
+    expenseOutput.innerText = parseFloat(expenseOutput.innerText) - expense.amountprop;
+    balanceOutput += expense.amountprop;
+    balance.innerText = balanceOutput;
+    allExpenses.splice(expenseIndex, 1)
+    addExpenseButton.innerText = "Update Expense"
+    row.remove();
+
     });
+
     editDeleteCell.appendChild(editButton);
 
+    
     var deleteButton = document.createElement("button");
     deleteButton.textContent = "Detele";
     deleteButton.addEventListener("click", function() {
         
-        balance.innerText = balanceOutput + expense.amountprop;
+        var expenseIndex = allExpenses.findIndex(exp => exp === expense);
+
+        balanceOutput += expense.amountprop;
+        balance.innerText = balanceOutput;
         expenseOutput.innerText = parseFloat(expenseOutput.innerText) - expense.amountprop;
-        
+        allExpenses.splice(expenseIndex, 1)
         row.remove();
 
     });
